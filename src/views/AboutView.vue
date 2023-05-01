2a6e9c6770658cbfb4ffec72abd07ed5714619c2
Hemant <yadavh542@gmail.com>
2023-05-01 20:23:51 +0530

    about changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,8 @@
 
 <template>
   <main class="about">
-    <h1>This is an about page</h1>
+    <h1>About My Vue App</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   </main>
 </template>
 
@@ -14,7 +15,7 @@ main {
   width: 100%;
   margin: 0 auto;
   padding: 40px 16px;
-  h1 {
+  h1,p {
     margin-bottom: 16px;
     text-align: center;
   }
