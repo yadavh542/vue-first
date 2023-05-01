e27e9e477cdf32c708094fa4f6fa269b8db126e0
Hemant <yadavh542@gmail.com>
2023-05-01 20:17:47 +0530

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <main class="about">
+    <h1>This is an about page</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
